7451421aa088ced7ecd9aac45bb3bcc8cc430aaf
enikop <90562775+enikop@users.noreply.github.com>
2023-11-13 18:32:23 +0100

    7. gyakorlat javítás

--- YD11NL_1108/DomParseYd11nl/src/domyd11nl1108/DomReadYd11nl.java
+++ YD11NL_1108/DomParseYd11nl/src/domyd11nl1108/DomReadYd11nl.java
@@ -11,7 +11,6 @@ import org.xml.sax.SAXException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class DomReadYd11nl {
@@ -32,87 +31,59 @@ public class DomReadYd11nl {
 		String indentStr = "   ";
 		int indent = 0;
 		Element root = document.getDocumentElement();
-		System.out.println(root.getNodeName());
+		System.out.println("<"+root.getNodeName()+">");
 		indent++;
 		NodeList lessons = root.getElementsByTagName("ora");
 		for(int i=0; i<lessons.getLength(); i++) {
 			Element lesson = (Element)lessons.item(i);
-			System.out.print(indentStr.repeat(indent)+"ora");
+			System.out.print(indentStr.repeat(indent)+"<ora");
 			NamedNodeMap attributes = lesson.getAttributes();
 			printAttributes(attributes);
 			
 			Element subject = (Element) lesson.getElementsByTagName("targy").item(0);
 			indent++;
-			System.out.println(indentStr.repeat(indent)+"tantargy");
-			indent++;
-			System.out.println(indentStr.repeat(indent)+subject.getTextContent());
-			indent--;
-			System.out.println(indentStr.repeat(indent)+"tantargy");
+			System.out.println(indentStr.repeat(indent)+"<targy>"+subject.getTextContent()+"</targy>");
 			
 			Element time = (Element) lesson.getElementsByTagName("idopont").item(0);
 			printTime(time, indent, indentStr);
 			
 			Element place = (Element) lesson.getElementsByTagName("helyszin").item(0);
-			System.out.println(indentStr.repeat(indent)+"helyszin");
-			indent++;
-			System.out.println(indentStr.repeat(indent)+place.getTextContent());
-			indent--;
-			System.out.println(indentStr.repeat(indent)+"helyszin");
+			System.out.println(indentStr.repeat(indent)+"<helyszin>"+place.getTextContent()+"</helyszin>");
 			Element teacher = (Element) lesson.getElementsByTagName("oktato").item(0);
-			System.out.println(indentStr.repeat(indent)+"oktato");
-			indent++;
-			System.out.println(indentStr.repeat(indent)+teacher.getTextContent());
-			indent--;
-			System.out.println(indentStr.repeat(indent)+"oktato");
+			System.out.println(indentStr.repeat(indent)+"<oktato>"+teacher.getTextContent()+"</oktato>");
 			Element major = (Element) lesson.getElementsByTagName("szak").item(0);
-			System.out.println(indentStr.repeat(indent)+"szak");
-			indent++;
-			System.out.println(indentStr.repeat(indent)+major.getTextContent());
+			System.out.println(indentStr.repeat(indent)+"<szak>"+major.getTextContent()+"</szak>");
 			indent--;
-			System.out.println(indentStr.repeat(indent)+"szak");
-			indent--;
-			System.out.println(indentStr.repeat(indent)+"ora");
+			System.out.println(indentStr.repeat(indent)+"</ora>");
 			
 		}
-		System.out.println(root.getNodeName());
+		System.out.println("</"+root.getNodeName()+">");
 	}
 	private static void printTime(Element time, int indent, String indentStr) {
-		System.out.println(indentStr.repeat(indent)+"idopont");
+		System.out.println(indentStr.repeat(indent)+"<idopont>");
 		Element day = (Element) time.getElementsByTagName("nap").item(0);
 		Element from = (Element) time.getElementsByTagName("tol").item(0);
 		Element to = (Element) time.getElementsByTagName("ig").item(0);
 		indent++;
-		System.out.println(indentStr.repeat(indent)+"nap");
-		indent++;
-		System.out.println(indentStr.repeat(indent)+day.getTextContent());
+		System.out.println(indentStr.repeat(indent)+"<nap>"+day.getTextContent()+"</nap>");
+		System.out.println(indentStr.repeat(indent)+"<tol>"+from.getTextContent()+"</tol>");
+		System.out.println(indentStr.repeat(indent)+"<ig>"+to.getTextContent()+"</ig>");
 		indent--;
-		System.out.println(indentStr.repeat(indent)+"nap");
-		System.out.println(indentStr.repeat(indent)+"tol");
-		indent++;
-		System.out.println(indentStr.repeat(indent)+from.getTextContent());
-		indent--;
-		System.out.println(indentStr.repeat(indent)+"tol");
-		System.out.println(indentStr.repeat(indent)+"ig");
-		indent++;
-		System.out.println(indentStr.repeat(indent)+to.getTextContent());
-		indent--;
-		System.out.println(indentStr.repeat(indent)+"ig");
-		indent--;
-		System.out.println(indentStr.repeat(indent)+"idopont");
+		System.out.println(indentStr.repeat(indent)+"</idopont>");
 	}
 	private static void printAttributes(NamedNodeMap attributes) {
 		if(attributes.getLength()==0) {
-			System.out.println();
+			System.out.println(">");
 		}
 		else {
-			System.out.print(" {");
+			System.out.print(" ");
 			for(int i=0; i<attributes.getLength(); i++) {
-				System.out.print(attributes.item(i).getNodeName()+"="+attributes.item(i).getNodeValue());
+				System.out.print(attributes.item(i).getNodeName()+"=\""+attributes.item(i).getNodeValue()+"\"");
 				if(i!=attributes.getLength()-1) {
-					System.out.print(", ");
+					System.out.print(" ");
 				}
 			}
-			System.out.println("}");
+			System.out.println(">");
 		}
 		
 	}
--- YD11NL_1108/DomParseYd11nl/src/domyd11nl1108/DomWriteYd11nl.java
+++ YD11NL_1108/DomParseYd11nl/src/domyd11nl1108/DomWriteYd11nl.java
@@ -1,26 +1,24 @@
 package domyd11nl1108;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class DomWriteYd11nl {
@@ -38,14 +36,36 @@ public class DomWriteYd11nl {
 			e.printStackTrace();
 		}
 	}
-	public static void writeDocumentToFile(Document document, File output) throws TransformerException {
+	public static void writeDocumentToFile(Document document, File output) throws TransformerException, IOException {
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer;
 		transformer = transformerFactory.newTransformer();
 		transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 		DOMSource source = new DOMSource(document);
 		StreamResult outFile = new StreamResult(output);
 		transformer.transform(source, outFile);
+		//üres sorok törlése
+		deleteBlankLines(output);
+	
+	}
+	
+	public static void deleteBlankLines(File output) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(output));
+		List<String> lines = new ArrayList<String>();
+		String line = reader.readLine();
+		while(line != null) {
+			if(!line.strip().isEmpty()) {
+				lines.add(line);
+			}
+			line = reader.readLine();
+		}
+		reader.close();
+		FileWriter writer = new FileWriter(output);
+		for(String s: lines) {
+			writer.write(s+"\n");
+		}
+		writer.close();
 	}
 
 }
