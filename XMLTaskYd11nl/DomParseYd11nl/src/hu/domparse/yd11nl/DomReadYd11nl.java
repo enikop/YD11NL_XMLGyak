88f5c94bddfa47a873890d370d2e45ffa92fcdf3
enikop <90562775+enikop@users.noreply.github.com>
2023-12-02 22:44:41 +0100

    Beadandó kiegészítés

--- XMLTaskYd11nl/DomParseYd11nl/src/hu/domparse/yd11nl/DomReadYd11nl.java
+++ XMLTaskYd11nl/DomParseYd11nl/src/hu/domparse/yd11nl/DomReadYd11nl.java
@@ -30,9 +30,12 @@ public class DomReadYd11nl {
 			StreamResult newXmlStream = new StreamResult(newXmlFile);
 			//XML dokumentum beolvasása
 			Document document = parseXML("XMLYd11nl.xml");
-			//Kiírás az output fájlba és a konzolra
+			//Kiírás az output fájlba
 			writeDocument(document, newXmlStream);
-			System.out.println("A beolvasott dokumentum:\n\n"+formatXML(document));	
+			//Kiírás konzolra általános XML kiíróval
+			System.out.println("A beolvasott dokumentum:\n\n"+formatXML(document));
+			//Kiírás konzolra feladatspecifikus kiíróval
+			//System.out.println("A beolvasott dokumentum:\n\n"+getStructuredDocument(document));
 		} catch (IOException | SAXException | ParserConfigurationException | TransformerException e) {
 			e.printStackTrace();
 		}
@@ -78,14 +81,243 @@ public class DomReadYd11nl {
 		transformer.transform(source, output);
 	}
 	
-	//DOM fa visszaadása XML-ben formázva
+	//Kutyaiskola dokumentum strukturált tartalának visszaadása (feladat specifikus beolvasó)
+	public static String getStructuredDocument(Document document) {
+		//PI
+		String output = "<?xml version=\""+document.getXmlVersion()+"\" encoding=\""+document.getXmlEncoding()+"\"?>\n";
+		//Gyökérelem és gyerekelemei
+		output += "<"+document.getDocumentElement().getNodeName()+">\n";
+		output += getStructuredDogs(document, 1);
+		output += getStructuredMemberships(document, 1);
+		output += getStructuredOwners(document, 1);
+		output += getStructuredTrainings(document, 1);
+		output += getStructuredParticipations(document, 1);
+		output += getStructuredTrainers(document, 1);
+		output += "\n</"+document.getDocumentElement().getNodeName()+">";
+		
+		return output;
+	}
+	
+	//Strukturált kutya elemek
+	private static String getStructuredDogs(Document document, int indent) {
+		String output = "";
+		//Végigiterálunk az összes kutyán
+		NodeList dogNodes = document.getElementsByTagName("kutya");
+		for(int i=0; i<dogNodes.getLength(); i++) {
+			Element dog = (Element) dogNodes.item(i);
+			//Ha van az elem előtt comment, kiírjuk
+			output += getComment(dog.getPreviousSibling(), indent);
+			//Attribútumok
+			String id = dog.getAttribute("kkód");
+			String owner = dog.getAttribute("gazdi");
+			String membership = dog.getAttribute("tagság");
+			//Gyerekelemek
+			Element name = (Element) dog.getElementsByTagName("név").item(0);
+			Element breed = (Element) dog.getElementsByTagName("fajta").item(0);
+			Element birthDate = (Element) dog.getElementsByTagName("szül_dátum").item(0);
+			Element gender = (Element) dog.getElementsByTagName("nem").item(0);
+			//Strukturált Stringbe írás
+			output += indent(indent)+"<kutya kkód=\""+id+"\" gazdi=\""+owner+"\" tagság=\""+membership+"\">\n";
+			indent++;
+			output += indent(indent)+getStructuredSimpleElement("név", name.getTextContent());
+			output += indent(indent)+getStructuredSimpleElement("fajta", breed.getTextContent());
+			output += indent(indent)+getStructuredSimpleElement("szül_dátum", birthDate.getTextContent());
+			output += indent(indent)+getStructuredSimpleElement("nem", gender.getTextContent());
+			indent--;
+			output += indent(indent)+"</kutya>\n";
+		}
+		return output;
+	}
+	
+	//Strukturált tagság elemek
+	private static String getStructuredMemberships(Document document, int indent) {
+		String output = "";
+		NodeList membershipNodes = document.getElementsByTagName("tagság");
+		for(int i=0; i<membershipNodes.getLength(); i++) {
+			Element membership = (Element) membershipNodes.item(i);
+			//Ha van az elem előtt comment, kiírjuk
+			output += getComment(membership.getPreviousSibling(), indent);
+			//Attribútumok
+			String id = membership.getAttribute("tagkód");
+			String active = membership.getAttribute("aktív");
+			//Ha van aktív attribútum, kiírjuk, egyébként üres String helyette
+			String activity = active.isEmpty() ? "" :" aktív=\""+active+"\"";
+			//Gyerekelemek
+			Element firstDay = (Element) membership.getElementsByTagName("kezdőnap").item(0);
+			Element rate = (Element) membership.getElementsByTagName("tarifa").item(0);
+			//Strukturált Stringbe írás
+			output += indent(indent)+"<tagság tagkód=\""+id+activity+">\n";
+			indent++;
+			output += indent(indent)+getStructuredSimpleElement("kezdőnap", firstDay.getTextContent());
+			output += indent(indent)+getStructuredSimpleElement("tarifa", rate.getTextContent());
+			indent--;
+			output += indent(indent)+"</tagság>\n";
+		}
+		return output;
+	}
+	
+	//Strukturált gazdi elemek
+	private static String getStructuredOwners(Document document, int indent) {
+		String output = "";
+		NodeList ownerNodes = document.getElementsByTagName("gazdi");
+		for(int i=0; i<ownerNodes.getLength(); i++) {
+			Element owner = (Element) ownerNodes.item(i);
+			//Ha van az elem előtt comment, kiírjuk
+			output += getComment(owner.getPreviousSibling(), indent);
+			//Attribútumok
+			String id = owner.getAttribute("gkód");
+			//Gyerekelemek
+			Element name = (Element) owner.getElementsByTagName("név").item(0);
+			Element contact = (Element) owner.getElementsByTagName("elérhetőség").item(0);
+			Element birthYear = (Element) owner.getElementsByTagName("szül_év").item(0);
+			//Strukturált Stringbe írás
+			output += indent(indent)+"<gazdi gkód=\""+id+"\">\n";
+			indent++;
+			output += indent(indent)+getStructuredSimpleElement("név", name.getTextContent());
+			output += getStructuredContact(contact, indent);
+			output += indent(indent)+getStructuredSimpleElement("szül_év", birthYear.getTextContent());
+			indent--;
+			output += indent(indent)+"</gazdi>\n";
+		}
+		return output;
+	}
+	
+	//Strukturált foglalkozás elemek
+	private static String getStructuredTrainings(Document document, int indent) {
+		String output = "";
+		NodeList trainingNodes = document.getElementsByTagName("foglalkozás");
+		for(int i=0; i<trainingNodes.getLength(); i++) {
+			Element training = (Element) trainingNodes.item(i);
+			//Ha van az elem előtt comment, kiírjuk
+			output += getComment(training.getPreviousSibling(), indent);
+			//Attribútumok
+			String id = training.getAttribute("fkód");
+			String trainer = training.getAttribute("vezető");
+			//Gyerekelemek
+			NodeList topics = training.getElementsByTagName("téma");
+			Element place = (Element) training.getElementsByTagName("helyszín").item(0);
+			Element time = (Element) training.getElementsByTagName("időpont").item(0);
+			Element day = (Element) time.getElementsByTagName("dátum").item(0);
+			Element begin = (Element) time.getElementsByTagName("kezdet").item(0);
+			Element end = (Element) time.getElementsByTagName("vég").item(0);
+			//Strukturált Stringbe írás
+			output += indent(indent)+"<foglalkozás fkód=\""+id+"\" vezető=\""+trainer+"\">\n";
+			indent++;
+			//A téma többszörös elem, mindegyik előfordulását kiírjuk
+			for(int j=0; j<topics.getLength(); j++) {
+				output += indent(indent)+getStructuredSimpleElement("téma", ((Element)topics.item(j)).getTextContent());
+			}
+			output += indent(indent)+getStructuredSimpleElement("helyszín", place.getTextContent());
+			//Az időpont gyerekelem kiírása elemtartalmával együtt
+			output += indent(indent)+"<időpont>\n";
+			indent++;
+			output += indent(indent)+getStructuredSimpleElement("dátum", day.getTextContent());
+			output += indent(indent)+getStructuredSimpleElement("kezdet", begin.getTextContent());
+			output += indent(indent)+getStructuredSimpleElement("vég", end.getTextContent());
+			indent--;
+			output += indent(indent)+"</időpont>\n";
+			indent--;
+			output += indent(indent)+"</foglalkozás>\n";
+		}
+		return output;
+	}
+
+	//Strukturált részvétel elemek
+	private static String getStructuredParticipations(Document document, int indent) {
+		String output = "";
+		NodeList participationNodes = document.getElementsByTagName("részvétel");
+		for(int i=0; i<participationNodes.getLength(); i++) {
+			Element participation = (Element) participationNodes.item(i);
+			//Ha van az elem előtt comment, kiírjuk
+			output += getComment(participation.getPreviousSibling(), indent);
+			//Attribútumok
+			String dog = participation.getAttribute("kutya");
+			String training = participation.getAttribute("foglalkozás");
+			//Gyerekelemek
+			Element evaluation = (Element) participation.getElementsByTagName("értékelés").item(0);
+			NodeList prize = participation.getElementsByTagName("jutalom");
+			//Strukturált Stringbe írás
+			output += indent(indent)+"<részvétel kutya=\""+dog+"\" foglalkozás=\""+training+"\">\n";
+			indent++;
+			output += indent(indent)+getStructuredSimpleElement("értékelés", evaluation.getTextContent());
+			//A jutalom elem opcionális, csak akkor írjuk ki, ha létezik
+			output += prize.getLength()==0 ? "" : indent(indent)+getStructuredSimpleElement("jutalom", ((Element)prize.item(0)).getTextContent());
+			indent--;
+			output += indent(indent)+"</részvétel>\n";
+		}
+		return output;
+	}
+	
+	//Strukturált tréner elemek
+	private static String getStructuredTrainers(Document document, int indent) {
+		String output = "";
+		NodeList trainerNodes = document.getElementsByTagName("tréner");
+		for(int i=0; i<trainerNodes.getLength(); i++) {
+			Element trainer = (Element) trainerNodes.item(i);
+			//Ha van az elem előtt comment, kiírjuk
+			output += getComment(trainer.getPreviousSibling(), indent);
+			//Attribútumok
+			String id = trainer.getAttribute("tkód");
+			//Gyerekelemek
+			Element name = (Element) trainer.getElementsByTagName("név").item(0);
+			Element contact = (Element) trainer.getElementsByTagName("elérhetőség").item(0);
+			NodeList expertise = trainer.getElementsByTagName("szakterület");
+			Element wage = (Element) trainer.getElementsByTagName("órabér").item(0);
+			//Strukturált Stringbe írás
+			output += indent(indent)+"<tréner tkód=\""+id+"\">\n";
+			indent++;
+			output += indent(indent)+getStructuredSimpleElement("név", name.getTextContent());
+			output += getStructuredContact(contact, indent);
+			//A szakterület elem opcionális, csak akkor írjuk ki, ha létezik
+			output += expertise.getLength() == 0 ? "" : indent(indent)+getStructuredSimpleElement("szakterület", expertise.item(0).getTextContent());
+			output += indent(indent)+getStructuredSimpleElement("órabér", wage.getTextContent());
+			indent--;
+			output += indent(indent)+"</tréner>\n";
+		}
+		return output;
+	}
+	
+	//Strukturált elérhetőség elem
+	private static String getStructuredContact(Element contact, int indent) {
+		String output = "";
+		//Gyerekelemek
+		NodeList emails = contact.getElementsByTagName("email");
+		Element phone = (Element)contact.getElementsByTagName("telefon").item(0);
+		//Strukturált Stringbe írás
+		output += indent(indent)+"<elérhetőség>\n";
+		indent++;
+		//Az email többszörös elem, minden előfordulását kiírjuk
+		for(int i=0; i<emails.getLength(); i++) {
+			output+=indent(indent)+getStructuredSimpleElement("email", ((Element)emails.item(i)).getTextContent());
+		}
+		output += indent(indent)+getStructuredSimpleElement("telefon", phone.getTextContent());
+		indent--;
+		output += indent(indent)+"</elérhetőség>\n";
+		return output;
+	}
+
+	//Szöveges tartalmú elem visszaadása strukturált formában
+	private static String getStructuredSimpleElement(String tag, String content) {
+		return "<"+tag+">"+content+"</"+tag+">\n";
+	}
+	
+	//Komment csomópont Stringbe írása strukturáltan
+	private static String getComment(Node examinedNode, int indent) {
+		String output = "";
+		if(examinedNode.getNodeType()==Node.COMMENT_NODE) {
+			output="\n"+indent(indent)+"<!--"+((Comment)examinedNode).getData()+"-->\n";
+		}
+		return output;
+	}
+	
+	//DOM fa visszaadása XML-ben formázva (általános XML beolvasó, színezett PI és kommentek)
 	public static String formatXML(Document document) {
 		//Prolog kiírva kék színnel, utána vissza feketére
 		String pi = "\u001B[34m<?xml version=\""+document.getXmlVersion()+"\" encoding=\""+document.getXmlEncoding()+"\" ?>\u001B[30m";
 		return pi+formatElement(document.getDocumentElement(), 0);
 	}
 	
-	//Adott XML csomópontnak és tartalmának strukturált Stringgé konvertálása formázott kiíráshoz
+	//Adott XML csomópontnak és tartalmának strukturált Stringgé konvertálása formázott kiíráshoz (általános XML beolvasó)
 	public static String formatElement(Node node, int indent) {
 		//Ha node nem elem, üres Stringgel térünk vissza
 		if (node.getNodeType() != Node.ELEMENT_NODE) {
